8701f364091e1f24d81767fbbf5baa6e6f75c60a
lilelr <lilelr@163.com>
2021-10-19 15:30:27 +0800

    lemon merged successfully

--- src/master/main.cpp
+++ src/master/main.cpp
@@ -79,6 +79,10 @@
 
 #include "version/version.hpp"
 
+// lemon
+#include <lemon/lp.h>
+using namespace lemon;
+
 using namespace mesos::internal;
 #ifndef __WINDOWS__
 using namespace mesos::internal::log;
@@ -131,6 +135,40 @@ using std::vector;
 
 int main(int argc, char** argv)
 {
+  // Create an instance of the default MIP solver class
+// (it will represent an "empty" problem at first)
+  Mip mip;
+// Add two columns (variables) to the problem
+  Mip::Col x1 = mip.addCol();
+  Mip::Col x2 = mip.addCol();
+// Add rows (constraints) to the problem
+//    mip.addRow(x1 - 5 <= x2);
+  mip.addRow(x1 + x2 == 14);
+  mip.addRow(0 <= 2 * x1 + x2 <= 25);
+
+// Set lower and upper bounds for the columns (variables)
+  mip.colLowerBound(x1, 0);
+  mip.colUpperBound(x2, 10);
+
+// Set the type of the columns
+  mip.colType(x1, Mip::INTEGER);
+  mip.colType(x2, Mip::REAL);
+
+// Specify the objective function
+  mip.max();
+  mip.obj(5 * x1 + 3 * x2);
+
+// Solve the problem using the underlying MIP solver
+  mip.solve();
+// Print the results
+  if (mip.type() == Mip::OPTIMAL) {
+    std::cout << "Objective function value: " << mip.solValue() << std::endl;
+    std::cout << "x1 = " << mip.sol(x1) << std::endl;
+    std::cout << "x2 = " << mip.sol(x2) << std::endl;
+  } else {
+    std::cout << "Optimal solution not found." << std::endl;
+  }
+
   // The order of initialization of various master components is as follows:
   // * Validate flags.
   // * Log build information.
